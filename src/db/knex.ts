2143621ede81fb798803310283b63952357f0f7f
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-11-07 00:14:44 +0900

    Update: Merging after pull request

--- src/db/knex.ts
+++
@@ -1,6 +0,0 @@
-import knex from 'knex';
-import config from "./knexfile";
-
-const environment = process.env.NODE_ENV || "development";
-
-export = knex(config[environment]);
--- src/knex.ts
+++
@@ -1,8 +0,0 @@
-import knex from "knex";
-import config from "../knexfile";
-
-const environment = process.env.NODE_ENV || "development";
-
-const db = knex(config["production"]);
-
-export default db;
