27aa1b08a0537cd83ff366f6c4c67cf84caa90ad
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-06 19:11:36 +0900

    Feat: Add Results Component

---
+++ client/cost_of_living/src/Components/Results.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export interface IResultsPageProps { 
+}
+ 
+const ResultsPage: React.FunctionComponent<IResultsPageProps> = (props) => {
+    return <div>
+        Results
+    </div>;
+};
+ 
+export default ResultsPage;
